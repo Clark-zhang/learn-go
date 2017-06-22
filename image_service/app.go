2349eddf2ecfc50af2c44aaee1597ed488ce3326
Yiquan Zhang <zhangyiquan@yuewen.com>
2017-06-22 17:43:11 +0800

    image service

--- image_service/app.go
+++ image_service/app.go
@@ -1,19 +1,30 @@
+//Usage:
+// mkdir -p /tmp/clark-log
+// go run app.go -path=/tmp/clark-service -port=7007 -log_dir=/tmp/clark-log &
+
+
+
 package main
 
 import (
-    "fmt"
     "net/http"
     "log"
+    "flag"
     "github.com/Clark-zhang/learn-go/image_service/router"
-    "github.com/Clark-zhang/learn-go/image_service/config"
+    "github.com/golang/glog"
+    controller "github.com/Clark-zhang/learn-go/image_service/controller"
 )
 
 func main() {
-    Config := config.GetConfig()
+    path := flag.String("path", "/tmp/learn-go/file_service/", "path to store files")
+    port := flag.String("port", "7007", "port the service will run")
+    flag.Parse()
 
-    fmt.Println("Server start with config: ")
-    fmt.Printf("%v", Config)
+    controller.Path = *path
 
     router.New()
-    log.Fatal(http.ListenAndServe(":7007", nil))
+
+    glog.Info("Server start with path" + *path + " on port" + *port)
+
+    log.Fatal(http.ListenAndServe(":" + *port, nil))
 }
--- image_service/controller/service.go
+++ image_service/controller/service.go
@@ -4,41 +4,47 @@ import(
     "fmt"
     "net/http"
     // "encoding/json"
-    "github.com/Clark-zhang/learn-go/image_service/config"
     // "bytes"
     "os"
     "io"
+    glog "github.com/golang/glog"
 )
+
+var Path string
 
 func AddImage(w http.ResponseWriter, r *http.Request) {
     //32 << 20 == 32 * 2^20 == 32M?
     r.ParseMultipartForm(32 << 20)
 
-    conf := config.GetConfig()
-
-    servicePath := conf.Folder + r.Form.Get("service") + "/"
+    servicePath := Path + "/" + r.Form.Get("service") + "/"
 
     file, handler, err := r.FormFile("file")
     if err != nil {
-       fmt.Println(err)
-       return
+        glog.Fatalf("FormFile: %s", err)
+        return
     }
     defer file.Close()
 
     // fmt.Fprintf(w, "%v", handler.Header)
 
+    if _, err := os.Stat(servicePath); os.IsNotExist(err) {
+       os.MkdirAll(servicePath, 0777)
+    }
+
     f, err := os.OpenFile(servicePath + handler.Filename, os.O_WRONLY|os.O_CREATE, 0777)
     if err != nil {
-       fmt.Println(err)
-       return
+        glog.Fatalf("OpenFile: %s", err)
+        return
     }
     defer f.Close()
 
     _, err = io.Copy(f, file)
 
     if err != nil {
+        glog.Fatalf("Copy: %s", err)
         fmt.Fprintf(w, "%s\n", "Fail")
     }else {
+        glog.Info("Sucess " + servicePath + handler.Filename)
         fmt.Fprintf(w, "%s\n", "Success")
     }
 
